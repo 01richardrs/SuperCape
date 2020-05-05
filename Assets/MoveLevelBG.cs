e01cb08af32d8d074c0f242c69394e7da9f6bd82
Richard S <45613155+01richardrs@users.noreply.github.com>
2020-05-05 21:53:32 +0800

    Add Player,Camera,Script,MovingBG,InvisiWall
    TODO:
    - Player need to know ground to implement proper jump.
    - Need Bg to proper Roll/BG not suitable.
    - Need Change Player Sprite & collider.
    - Level not yet Moving & no obstacle.
    
    Done:
    - Add Movement Player Script & Gravity
    - Camera Following Player
    - Invisible Wall to prevent player pass through.
    - Move Script for BG

---
+++ Assets/MoveLevelBG.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLevelBG : MonoBehaviour
+{
+    public float speed;
+    public float StartX;
+    public float endX;
+
+    void Update()
+    {
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+
+        if (transform.position.x <= endX)
+        {
+            Vector2 pos = new Vector2(StartX, transform.position.y);
+            transform.position = pos;
+        }
+    }
+}
---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public Collider2D coll2D;
+    //public Animator animator;
+    //private AudioSource audio;
+    //public Text Score;
+
+    public float gravity = 1f;
+    public float maxSpeed = 1f;
+    public float SpeedMultiplier;
+    public float Speed= 10f;
+    public float JumpHeight=10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        //audio = GetComponent<AudioSource>();
+        //animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horizontalAxis = Input.GetAxis("Horizontal");
+        float verticalAxis = Input.GetAxis("Vertical");
+        float jump = Input.GetAxis("Jump");
+        float rbVelocity = rb.velocity.magnitude;
+        if (rbVelocity < maxSpeed)
+        {
+            rb.AddForce(new Vector2(horizontalAxis * Speed, 0));
+        }
+
+        if (horizontalAxis < 0 && this.transform.localScale.x == 1f)
+        {
+            transform.localScale = new Vector2(-1f, 1f);
+        }
+        else if (horizontalAxis > 0 && this.transform.localScale.x == -1f)
+        {
+            transform.localScale = new Vector2(1f, 1f);
+        }
+        if (jump >= 0.02)
+        {
+            rb.AddForce(new Vector2(0, 23 * JumpHeight));
+        }
+        
+    }
+}
