2a5a7905a141808424909e41aa584ded461d8e0a
Aldrich Widjaja <aldrichwidjaja@gmail.com>
2020-05-07 21:13:33 +0700

    FlyingEnemy deployment script

---
+++ Assets/Script/DeployEnemy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeployEnemy : MonoBehaviour
+{
+    public GameObject EnemyPrefab;
+    public float respawnTime = 1.0f;
+    private Vector2 screenBounds;
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, Camera.main.transform.position.z));
+        StartCoroutine(enemyWave());
+    }
+
+    private void spawnEnemy()
+    {
+        GameObject a = Instantiate(EnemyPrefab) as GameObject;
+        a.transform.position = new Vector2(screenBounds.x * -2, Random.Range(-screenBounds.y, screenBounds.y));
+    }
+
+    IEnumerator enemyWave()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(respawnTime);
+            spawnEnemy();
+        }
+        
+    }
+}
