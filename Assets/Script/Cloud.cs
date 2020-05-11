084f5426ce991c9ee5bfd229a07f8fa3595f2b10
Richard S <45613155+01richardrs@users.noreply.github.com>
2020-05-11 14:15:52 +0800

    Add Clouds/ Tidy up Folders
    - Tidying up folders.
    - Add Moving clouds

---
+++ Assets/Script/Cloud.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cloud : MonoBehaviour
+{
+    public float speed = 0.1f;
+ 
+    void Update()
+    {
+        transform.Translate((Vector3.left * speed) * Time.deltaTime);
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Deleter")
+        {
+            //this.GetComponent<Renderer>().enabled = false;
+            StartCoroutine(BeforeDestroyed());
+        }
+    }
+
+    IEnumerator BeforeDestroyed()
+    {
+        yield return new WaitForSeconds(1);
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Script/CloudSpawner.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudSpawner : MonoBehaviour
+{
+    public GameObject CloudPrefab0;
+    public GameObject CloudPrefab1;
+    public GameObject CloudPrefab2;
+    public GameObject CloudPrefab3;
+    public float respawnTime = 8f;
+    private Vector2 screenBounds;
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, Camera.main.transform.position.z));
+        StartCoroutine(CloudSpawn());
+        //StartCoroutine(CloudSpawn());
+    }
+
+
+    private void Randomizer()
+    {
+        var randnum = Random.Range(0, 100);
+
+        if (randnum > 80)
+        {
+            GameObject ab = Instantiate(CloudPrefab1) as GameObject;
+            ab.transform.position = new Vector2(screenBounds.x * -2, Random.Range(1.7f, 4.6f));
+        }
+        else if (randnum > 60)
+        {
+            GameObject abcd = Instantiate(CloudPrefab3) as GameObject;
+            abcd.transform.position = new Vector2(screenBounds.x * -2, Random.Range(1.7f, 4.6f));
+        }
+        else if (randnum > 40)
+        {
+            GameObject a = Instantiate(CloudPrefab0) as GameObject;
+            a.transform.position = new Vector2(screenBounds.x * -2, Random.Range(1.7f, 4.6f));
+        }else
+        {
+            GameObject abc = Instantiate(CloudPrefab2) as GameObject;
+            abc.transform.position = new Vector2(screenBounds.x * -2, Random.Range(1.7f, 4.6f));
+        }
+
+    }
+
+    IEnumerator CloudSpawn()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(respawnTime);
+            Randomizer();
+
+            yield return new WaitForSeconds(0.5f);
+
+            yield return new WaitForSeconds(respawnTime);
+            Randomizer();
+        }
+        
+    }
+}
