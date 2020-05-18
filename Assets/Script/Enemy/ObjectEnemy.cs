4387f394a299f11b0d88ac9fc9d5820f17ece598
Richard S <45613155+01richardrs@users.noreply.github.com>
2020-05-18 21:27:36 +0800

    Add Enemy Object, Enemy Object Pattern, Fix Invisible on Object, BeachGround
    - Add 5 Enemy Object with basic script
    - Add Enemy Object Pattern [For spawn patterned object]
    - Fix invisible behavior on Object
    - Add Beach Ground

---
+++ Assets/Script/Enemy/ObjectEnemy.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectEnemy : MonoBehaviour
+{
+    public bool PlayerInvisible = false;
+    public bool PlayerGiant = false;
+    public GameObject Player;
+    public BoxCollider2D Bcoll2D;
+    public CircleCollider2D Ccoll2D;
+
+    private void Start()
+    {
+        Bcoll2D = GetComponent<BoxCollider2D>();
+        Ccoll2D = GetComponent<CircleCollider2D>();
+    }
+    private void Update()
+    {
+        PlayerInvisible = Player.GetComponent<PlayerMovement>().InvicibleStatus;
+        PlayerGiant = Player.GetComponent<PlayerMovement>().GiantStatus;
+
+        if (PlayerInvisible)
+        {
+            Bcoll2D.enabled = false;
+            Ccoll2D.enabled = false;
+        }
+        else {
+            Bcoll2D.enabled = true;
+            Ccoll2D.enabled = true;
+        }
+        if (PlayerGiant)
+        {
+            //dosomething
+        }
+        else
+        {
+            //dosomething
+        }
+
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Deleter")
+        {
+            //this.GetComponent<Renderer>().enabled = false;
+            StartCoroutine(BeforeDestroyed());
+        }
+    }
+
+    IEnumerator BeforeDestroyed()
+    {
+        yield return new WaitForSeconds(1);
+        Destroy(this.gameObject);
+    }
+}
