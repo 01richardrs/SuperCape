f601a61b37a30d4f7f7d003b6f2afef7001a1aa1
wilsonGoei <wilson.gujatno@my.jcu.edu.au>
2020-05-20 11:11:44 +0800

    Add health system

---
+++ Assets/Script/Player/HealthSystem.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthSystem : MonoBehaviour
+{
+    public int healthNum;
+
+    public Image[] hearts;
+    public Sprite redHeart;
+
+    GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hearts.Length < 1)
+        {
+            //Debug.Log("lebu");
+            //this.enabled = false;
+        }
+
+        // buat display jumlah health, tergantung ama health num
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if (i < healthNum)
+            {
+                hearts[i].enabled = true;
+            }
+            else
+            {
+                hearts[i].enabled = false;
+            }
+        }    
+
+    }
+
+    public void HealthLoss()
+    {
+        healthNum--;
+    }
+}
