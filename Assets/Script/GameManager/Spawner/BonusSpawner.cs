f910a83bcf4c601adeb7904c18c33deeb87ca01b
Richard S <45613155+01richardrs@users.noreply.github.com>
2020-05-22 18:09:16 +0800

    Add Bonus Spawners
    - Changed name SpawnMain to Game Manager at Prefab
    - Add Bonus Spawner based on time played that spawns at random Ypos

---
+++ Assets/Script/GameManager/Spawner/BonusSpawner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BonusSpawner : MonoBehaviour
+{
+    float BonusTime;
+    public GameObject Giant_Bonus;
+    public GameObject Invisible_Bonus;
+    public GameObject Health_Bonus;
+    void Update()
+    {
+        if (!(Time.timeScale == 0))
+        {
+            BonusTime += Time.deltaTime;
+            if (BonusTime > 75)
+            {
+                var randY = Random.Range(-2.4f, 3.50f);
+                var randnum = Random.Range(0, 2);
+                Debug.Log("Bonus Is Coming");
+                switch (randnum)
+                {
+                    
+                    case 0:
+                        GameObject Bonus0 = Instantiate(Giant_Bonus, new Vector2(10.2f, randY), Quaternion.identity) as GameObject;
+                        break;
+                    case 1:
+                        GameObject Bonus1 = Instantiate(Invisible_Bonus, new Vector2(10.2f, randY), Quaternion.identity) as GameObject;
+                        break;
+                    case 2:
+                        GameObject Bonus2 = Instantiate(Health_Bonus, new Vector2(10.2f, -randY), Quaternion.identity) as GameObject;
+                        break;
+                }
+                BonusTime = 0;
+            }
+            
+        }
+    }
+}
--- Assets/Script/GameManager/SpeedMultiplier.cs
+++ Assets/Script/GameManager/SpeedMultiplier.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class SpeedMultiplier : MonoBehaviour
 {
     public GameObject PauseMenu;
-    float playedTime;
+    public float playedTime;
     public float BasicSpeed = 0.8f;
     // Start is called before the first frame update
     void Start()
